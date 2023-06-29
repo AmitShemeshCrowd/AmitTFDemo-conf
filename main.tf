e965971de0c5842bd951f2b844bfae661eb5e2e4
ashemesh <ashemesh@akamai.com>
2023-06-29 10:48:47 +0300

    my second commit

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 module "Amit" {
-  source = "/Users/ashemesh/Desktop/AmitTFDemo"
+  #source = "/Users/ashemesh/Desktop/AmitTFDemo"
+  source = "git@github.com:AmitShemeshCrowd/AmitTFDemo.git?ref=main"
   ec2_instance_type = "t2.medium"
 }
